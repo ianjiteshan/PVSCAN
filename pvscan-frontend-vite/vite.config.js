6b0f958a9a39e82db6f14bb4e7badd8dd89900ee
ianjiteshan <anjitesh1976@gmail.com>
2025-08-31 08:05:44 +0530

    CORS

--- pvscan-frontend-vite/vite.config.js
+++ pvscan-frontend-vite/vite.config.js
@@ -1,9 +1,14 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  // for same route as backend lol
-  base: '/',
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [vue()],
+    base: '/',
+    define: {
+      __API_BASE_URL__: JSON.stringify(env.VITE_API_BASE_URL),
+    }
+  };
 })
